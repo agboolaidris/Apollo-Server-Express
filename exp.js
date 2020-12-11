83f236fcbeba5dc1568545b5ccba4f128d0abfb7
Agboolaidris <agboolaisholaidreez@gmail.com>
2020-12-11 07:55:28 +0100

    update files

--- exp.js
+++ exp.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
+app.set('view engine','ejs')
 
 app.get('/', (req,res)=>{
-  res.send('I am learning programinng')
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/contact',(req,res)=>{
+  res.sendFile(__dirname + '/contact.html')
+})
+
+app.get('/profile/:id',(req,res)=>{
+    const data = {
+      age:12,
+      job:'MERN',
+      hobbies:['reading','coding','sleeping']
+    }
+    res.render('profile',{person:req.params.id, data:data})
 })
 
 app.listen(3000)
