5f45d62147010a2b6608c1ecdea9fa6c4f4980ce
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-10 15:25:33 +0100

    update

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,18 @@
 const form = document.querySelector("#form");
 const input = document.querySelector("#input");
-const error = document.querySelector("#error");
-const message = document.querySelector("#message");
+const message1 = document.querySelector("#error");
+const message2 = document.querySelector("#message");
 
 const axios = (location) => {
+  message1.textContent = "loading .....";
   fetch(`http://localhost:3000/weather?address=${location}`).then((res) => {
     res.json().then((data) => {
       if (data.error) {
-        error.textContent = data.error;
-        console.log(data.error);
+        message1.textContent = data.error;
       } else {
         console.log(data);
+        message1.textContent = `The city of `;
+        message2.textContent = data.location;
       }
     });
   });
