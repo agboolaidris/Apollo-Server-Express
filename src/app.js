a2dc3097e3e16af323b972f82f8544dce1b44b9c
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-10 10:58:44 +0100

    update

--- src/app.js
+++ src/app.js
@@ -1,29 +1,33 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 const app = express();
 
 //setup handlebar engine and view location
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "../template/views"));
-
+hbs.registerPartials(path.join(__dirname, "../template/partials"));
 //setup static directory to serve
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
   res.render("index", {
     title: "weather app",
-    developer: "Idris Agboola",
+    developer: "cristiano Ronaldo",
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", {
-    title: "weather app",
+    title: "About Page!!!",
     developer: "Idris Agboola",
   });
 });
 app.get("/help", (req, res) => {
-  res.render("help");
+  res.render("help", {
+    title: "Help Page!!!",
+    developer: "David Beckham",
+  });
 });
 
 app.listen(3000, () => {
