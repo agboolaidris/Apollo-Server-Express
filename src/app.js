1beca6733311504730b5a1abbfb4803494c424d9
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-10 01:12:36 +0100

    update files

--- src/app.js
+++ src/app.js
@@ -7,7 +7,19 @@ app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    title: "weather app",
+    developer: "Idris Agboola",
+  });
+});
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "weather app",
+    developer: "Idris Agboola",
+  });
+});
+app.get("/help", (req, res) => {
+  res.render("help");
 });
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
