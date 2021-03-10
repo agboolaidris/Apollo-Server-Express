b91b8bf04fad1d0cedd4e94a1c2f937c28c8eb34
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-10 14:19:04 +0100

    update

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,7 @@
-console.log("hello");
+console.log("hhhhhhhhhhhhhhh");
+alert("hello");
+fetch("https://localhost:3000/weather").then((res) => {
+  res.json().then((data) => {
+    console.log(data);
+  });
+});
